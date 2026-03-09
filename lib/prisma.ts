b09c6b2caf456312f797f7ea6c5dc5b97de43cfd
Dawid <dawid.piecko@gmail.com>
2026-03-10 00:54:24 +0100

    fixed DATABASE_URL env variable

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,7 +6,7 @@ const globalForPrisma = global as unknown as {
 };
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DEV_DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 const prisma =
